338766ac65724e472d42787e18c20a0214ffcf2e
Zach Bray <zachbray@googlemail.com>
2016-12-05 00:44:49 +0000

    Added dist folder for packaging.

---
+++ dist/src/Calculator.js
@@ -0,0 +1,12 @@
+"use strict";
+var Calculator = (function () {
+    function Calculator() {
+    }
+    Calculator.prototype.add = function (x, y) {
+        return x + y;
+    };
+    return Calculator;
+}());
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = Calculator;
+//# sourceMappingURL=Calculator.js.map
